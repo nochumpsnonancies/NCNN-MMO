6fd45fda18f02cecb915f51633ecc48e2e49c8b0
dtoillion <toillion@gmail.com>
2016-09-27 00:58:14 -0700

    basic networked player objects

---
+++ NCNN-MMO/Assets/_Scripts/NetworkPlayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class NetworkPlayer : NetworkBehaviour {
+
+	void Start ()
+  {}
+
+	void Update ()
+  {
+    if (!isLocalPlayer)
+    {
+      return;
+    }
+	}
+}
